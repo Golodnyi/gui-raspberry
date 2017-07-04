95ddcee3e269fa3265ec241f45c98500f34ddabc
golodnyi <ochen@golodnyi.ru>
2017-07-04 10:41:15 +0700

    linux support

--- ConsoleApplication3/Func.cpp
+++ ConsoleApplication3/Func.cpp
@@ -1,12 +1,12 @@
+#if defined(WIN32) || defined(WINDOWS)
 #include "stdafx.h";
+#endif
 
-unsigned char xor_sum(unsigned char *buffer, unsigned int length)
-{
-	unsigned char temp_sum = 0;
+unsigned char xor_sum(unsigned char *buffer, unsigned int length) {
+    unsigned char temp_sum = 0;
 
-	while (length-- > 0)
-	{
-		temp_sum ^= *buffer++;
-	}
-	return (temp_sum);
+    while (length-- > 0) {
+        temp_sum ^= *buffer++;
+    }
+    return (temp_sum);
 }
