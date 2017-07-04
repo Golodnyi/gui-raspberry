21143aef081ee17db24bb94328cf9ec6138b65fb
golodnyi <ochen@golodnyi.ru>
2017-07-04 13:45:08 +0700

    cnake cache

---
+++ cmake-build-debug/CMakeFiles/guiraspberry.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/guiraspberry.dir/ConsoleApplication3/ConsoleApplication3.cpp.o"
+  "guiraspberry.pdb"
+  "guiraspberry"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/guiraspberry.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
