dadd7bcd5c9300364555a1d1745452e9b29e549e
Angelina <a.esaulenko2013@yandex.ru>
2017-08-04 14:47:27 +0700

    wiringPi

--- src/gui/gpio.cpp
+++ src/gui/gpio.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-#include <pigpio.h>
+#include <wiringPi.h>
 
-//pin
+#define PIN 27
 
 using namespace std;
 
@@ -9,28 +9,28 @@ int gpio(void){
     cout << "gpio start"<< endl;
     unsigned int tone[8] = {956, 851, 758, 716, 638, 568, 506, 478}; // Создаём массив с рассчитанными периодами для каждой ноты
 
-    if (!gpioInitialise()) {    // Инициализация GPIO
-        cout << "bcm2835 no found" << endl;
+    if (!wiringPiSetup ()) {    // Инициализация GPIO
+        cout << "not found" << endl;
         return 1;
     }           //Завершение программы, если инициализация не удалась
 
-    gpioRead(16);     // Устанавливаем порт на вывод
+    pinMode (PIN, OUTPUT) ;     // Устанавливаем порт на вывод
 
     int i=8, j;             // Объявляем переменные. i- количество нот, j- продолжительность звучания ноты.
 
     while (i)  // Играем ноты пока i не равно 0
     {
-        gpioDelay(200);      // Просто пауза перед проигрыванием каждой ноты
+        delay(200);      // Просто пауза перед проигрыванием каждой ноты
         for (j=600; j!=0; j--)   // 600 раз повторяем колебания для каждой ноты (примерно 1 секунда звучания)
         {
-            gpioWrite(16, 0x0);
-            gpioDelay(tone[i-1]);
-            gpioWrite(16, 0x1);
-            gpioDelay(tone[i-1]);
+            digitalWrite(PIN, 0x0);
+            delay(tone[i-1]);
+            digitalWrite(PIN, 0x1);
+            delay(tone[i-1]);
         }
         i--;
     }
-    return (!gpioSerialReadClose); // Выход из программы
+    return (0); // Выход из программы
 }
 
 
