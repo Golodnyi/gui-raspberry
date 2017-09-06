e3fdd902470ca0fdf8d07b4f1d0faaa5ed0e3c7e
golodnyi <ochen@golodnyi.ru>
2017-09-06 15:17:12 +0700

    потоки для звука, рефакторинг

--- src/gui/draw.cpp
+++ src/gui/draw.cpp
@@ -6,7 +6,8 @@
 
 using namespace std;
 
-extern int gpio(dataStruct *telemetry_values, int i, bool sound);
+extern void *warning(void *arg);
+extern void *danger(void *arg);
 
 int color(dataStruct *telemetry_values, int i, QListWidgetItem *Item,
           bool sound) {
@@ -15,16 +16,11 @@ int color(dataStruct *telemetry_values, int i, QListWidgetItem *Item,
       telemetry_values[i].problemTime = time(NULL);
     }
 
-    Item->setText(
-      telemetry_values[i].name + ": " + telemetry_values[i].value +
-      " " + telemetry_values[i].unit + "\n" +
-      QString::fromStdString(
-        to_string(
-          (int)difftime(time(NULL), telemetry_values[i].problemTime)
-        )
-      ) +
-      " сек. Назад"
-    );
+    Item->setText(telemetry_values[i].name + ": " + telemetry_values[i].value +
+                  " " + telemetry_values[i].unit + "\n" +
+                  QString::fromStdString(to_string((int)difftime(
+                      time(NULL), telemetry_values[i].problemTime))) +
+                  " сек. Назад");
     Item->setBackground(Qt::red);
   } else if (telemetry_values[i].color == "yellow") {
     Item->setBackground(Qt::yellow);
@@ -36,5 +32,27 @@ int color(dataStruct *telemetry_values, int i, QListWidgetItem *Item,
   if (telemetry_values[i].color != "red" && telemetry_values[i].problemTime) {
     telemetry_values[i].problemTime = 0;
   }
-  gpio(telemetry_values, i, sound);
+
+  if (sound) {
+    pthread_t danger_thread;
+    pthread_t warning_thread;
+
+    if (telemetry_values[i].color == "red") {
+      int result = pthread_create(&danger_thread, NULL, danger, NULL);
+      {
+        if (result != 0) {
+          return EXIT_FAILURE;
+        }
+        pthread_detach(danger_thread);
+      }
+    } else if (telemetry_values[i].color == "yellow") {
+      int result = pthread_create(&warning_thread, NULL, warning, NULL);
+      {
+        if (result != 0) {
+          return EXIT_FAILURE;
+        }
+        pthread_detach(warning_thread);
+      }
+    }
+  }
 }
--- src/gui/gpio.cpp
+++ src/gui/gpio.cpp
@@ -9,10 +9,7 @@ extern int PIN;
 
 using namespace std;
 
-int gpio(dataStruct *telemetry_values, int i, bool sound) {
-  if (!sound) {
-    return 0;
-  }
+void *warning(void *arg) {
 #if defined(__arm__)
   if (wiringPiSetup() == -1) { // Инициализация GPIO
     cout << "not found " << endl;
@@ -21,18 +18,29 @@ int gpio(dataStruct *telemetry_values, int i, bool sound) {
 
   pinMode(PIN, OUTPUT); // Устанавливаем порт на вывод
 
-  if (telemetry_values[i].color == "red") {
-    digitalWrite(PIN, HIGH);
-    delay(1000);
-    digitalWrite(PIN, LOW);
-  } else if (telemetry_values[i].color == "yellow") {
-    digitalWrite(PIN, HIGH);
-    delay(300);
-    digitalWrite(PIN, LOW);
+  digitalWrite(PIN, HIGH);
+  delay(300);
+  digitalWrite(PIN, LOW);
+  exit (0); // Выход из программы
+#else
+  exit (1);
+#endif
+}
+
+void *danger(void *arg) {
+#if defined(__arm__)
+  if (wiringPiSetup() == -1) { // Инициализация GPIO
+    cout << "not found " << endl;
+    return 1; //Завершение программы, если инициализация не удалась
   }
 
-  return (0); // Выход из программы
+  pinMode(PIN, OUTPUT); // Устанавливаем порт на вывод
+
+  digitalWrite(PIN, HIGH);
+  delay(1000);
+  digitalWrite(PIN, LOW);
+  exit (0); // Выход из программы
 #else
-  return (1);
+  exit (1);
 #endif
 }
