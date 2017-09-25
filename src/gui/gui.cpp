cbf073a4ce6d803c2f8298eb24b79cba3ed656b6
golodnyi <ochen@golodnyi.ru>
2017-09-25 16:36:20 +0700

    fix

--- src/gui/gui.cpp
+++ src/gui/gui.cpp
@@ -98,7 +98,7 @@ int gui_init(int argc, char *argv[])
   MainLayout->addWidget(rightWidget, 0, 1);
   MainLayout->addWidget(statusBar, 1, 0);
 
-  window->setContentsMargins(56, 0, 0, 120);
+  window->setContentsMargins(14, 40, 14, 30);
   window->setLayout(MainLayout);
   window->showFullScreen();
 
